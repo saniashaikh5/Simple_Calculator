28f85aa6f59a38de0f38df919555b0eaca07e572
saniashaikh5 <hurmen41@gmail.com>
2024-03-24 07:22:59 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//printing a Wellcome Message
+console.log("\n\tWellcome to \'sania_shaikh\' - CLI Simple Calculator\n");
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number" , name: "firstNumber"},
+    {message: "Enter Second Number" , type: "number" , name: "secondNumber"},
+    {
+        message: "Select One Operator To Perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Divition"],
+    },
+]);
+
+//Conditional statements IF_ELSE
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Divition"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+    console.log("please select valid operator");
+}
